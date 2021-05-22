d1f61d25ea44bf921e1bdac48c149bb02c9d46b4
niki0718 <70174008+niki0718@users.noreply.github.com>
2021-05-22 19:07:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// Prevent dropdown menu from closing when click inside the form
+$(document).on("click", ".action-buttons .dropdown-menu", function(e){
+	e.stopPropagation();
+});
+
+$(function() {
+	$('.nav-tabs').responsiveTabs();
+});
